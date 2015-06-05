98b43dd54603be9a9e5c1778e435ebdb1f4f08f8
kbendelow <kbendelow@me.com>
2015-06-05 11:20:32 -0500

    fixed dragging and disabled dragging

--- Classes/Utilities/CustomCameraView.m
+++ Classes/Utilities/CustomCameraView.m
@@ -1107,10 +1107,7 @@
         {
             CGPoint save = [point locationInView:self.view];
             CGPoint temp = CGPointMake(self.x3.center.x, self.x1.center.y);
-//            CGPoint *tempPoint = CGPointMake(self.x1.center.x, save.y);
             CGPoint new = CGPointMake(temp.x, save.y);
-            NSLog(@"you'retouchingme");
-//            [self.movingImage setCenter:[point locationInView:self.view]];
             [self.movingImage setCenter:new];
             [self insertImageInSlot];
         }
