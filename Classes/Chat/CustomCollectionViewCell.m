ca26f1c8ced59fb7cedc11b49ba56b28630428c3
Kyle <kbendelow@me.com>
2015-10-19 11:50:55 -0500

    initials on right side

--- Classes/Chat/CustomCollectionViewCell.m
+++ Classes/Chat/CustomCollectionViewCell.m
@@ -50,15 +50,31 @@
         self.selectedBackgroundView = backgroundView;
 
         // set content view
-        CGRect frame  = CGRectMake(self.bounds.origin.x + 5, self.bounds.origin.y+ 5, self.bounds.size.width, self.bounds.size.height);
-        self.imageView = [[PFImageView alloc] initWithFrame:frame];
+//        CGRect frame  = CGRectMake(self.bounds.origin.x + 5, self.bounds.origin.y+ 5, self.bounds.size.width, self.bounds.size.height);
+//        self.imageView = [[PFImageView alloc] initWithFrame:frame];
+//        self.imageView.contentMode = UIViewContentModeScaleAspectFill;
+//        self.imageView.clipsToBounds = YES;
+//        [self.contentView addSubview:self.imageView];
+//
+//        CGRect frame2 = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, 28, 28);
+//        self.label = [[UILabel alloc] initWithFrame:frame2];
+//        self.label.layer.cornerRadius = self.bounds.size.width/3.5/2;
+//        self.label.layer.masksToBounds = 1;
+//        self.label.layer.borderColor = [[UIColor whiteColor]CGColor];
+//        self.label.layer.borderWidth = 1;
+//        self.label.textAlignment = NSTextAlignmentCenter;
+//        self.label.font = [UIFont fontWithName:@"Helvetica Bold" size:12];
+//        self.label.textColor = [UIColor whiteColor];
+//        [self insertSubview:self.label aboveSubview:self.imageView];
+        
+        CGRect picFrame  = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, self.bounds.size.width, self.bounds.size.height);
+        self.imageView = [[PFImageView alloc] initWithFrame:picFrame];
         self.imageView.contentMode = UIViewContentModeScaleAspectFill;
         self.imageView.clipsToBounds = YES;
         [self.contentView addSubview:self.imageView];
-
-
-        CGRect frame2 = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, 28, 28);
-        self.label = [[UILabel alloc] initWithFrame:frame2];
+        
+        CGRect initialsFrame = CGRectMake(self.bounds.size.width - 25, self.bounds.origin.y, 28, 28);
+        self.label = [[UILabel alloc] initWithFrame:initialsFrame];
         self.label.layer.cornerRadius = self.bounds.size.width/3.5/2;
         self.label.layer.masksToBounds = 1;
         self.label.layer.borderColor = [[UIColor whiteColor]CGColor];
@@ -67,7 +83,6 @@
         self.label.font = [UIFont fontWithName:@"Helvetica Bold" size:12];
         self.label.textColor = [UIColor whiteColor];
         [self insertSubview:self.label aboveSubview:self.imageView];
-
     }
     return self;
 }
